80335b2a96a56d71ab6cdd20cb9d04e8c781a00e
Metaroygo <110649690+Metaroygo@users.noreply.github.com>
2024-08-23 23:40:47 +0800

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "98b9c4321c4f236110069ba211e8eb3a",
+const RESOURCES = {"flutter_bootstrap.js": "71eb5dc54cd69cd6a0f1eae774a90ac3",
 "version.json": "77d1f46ca99149deee3089306e07e1f2",
 "index.html": "516c948694ca2b42f211b542eac15866",
 "/": "516c948694ca2b42f211b542eac15866",
-"main.dart.js": "c5a97cede500f55fe4864c49a7823be9",
+"main.dart.js": "d646161d61c00f4fa9701d3158919f0e",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
